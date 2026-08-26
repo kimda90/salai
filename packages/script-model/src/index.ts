0b0efdefc01fa20fa6b2fce182801ce419ef99e2
Daniel Kim <kimda90@gmail.com>
2026-08-26 10:04:59 -0300

    feat(script-model): export operations fixtures duration and serialization

--- packages/script-model/src/index.ts
+++ packages/script-model/src/index.ts
@@ -1,3 +1,7 @@
+export * from "./duration.js";
 export * from "./factory.js";
+export * from "./fixtures.js";
+export * from "./operations.js";
+export * from "./serialization.js";
 export * from "./types.js";
 export * from "./validation.js";
