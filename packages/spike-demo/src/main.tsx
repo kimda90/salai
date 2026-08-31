933d878c98b3b215277741b90d5a1060049ee36e
Daniel Kim <kimda90@gmail.com>
2026-08-31 10:09:09 -0300

    feat: enable optional local machine bridge

--- packages/spike-demo/src/main.tsx
+++ packages/spike-demo/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { SalaiController, SalaiProvider } from "./controller";
+import { bridgeUrlFromLocation, startMachineBridge } from "./machine-bridge";
 import "./style.css";
 import "./av-script.css";
 import "./paper-edit.css";
@@ -10,6 +11,8 @@ const rootElement = document.querySelector<HTMLElement>("#app");
 if (!rootElement) throw new Error("Missing #app root");
 
 const controller = new SalaiController("product");
+const bridgeUrl = bridgeUrlFromLocation(window.location);
+if (bridgeUrl) startMachineBridge(controller, bridgeUrl);
 
 createRoot(rootElement).render(
   <StrictMode>
